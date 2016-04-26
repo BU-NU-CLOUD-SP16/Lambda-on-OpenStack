5660ea6f093c16d13724be8ffe8e84cd3776a19c
Sushant Mimani <sushant.mimani@gmail.com>
2016-04-26 12:26:08 +0000

    Scrips updates

--- install/ruby-package-installer.sh
+++ install/ruby-package-installer.sh
@@ -4,7 +4,7 @@
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 #\curl -sSL https://get.rvm.io | bash -s stable --ruby
 \curl -sSL https://get.rvm.io | bash
-`source /home/ubuntu/.rvm/scripts/rvm` 
+sudo `source /home/ubuntu/.rvm/scripts/rvm` 
 rvm install 2.3-dev
 #rvm use 2.3.0-dev
 gem install bundler
