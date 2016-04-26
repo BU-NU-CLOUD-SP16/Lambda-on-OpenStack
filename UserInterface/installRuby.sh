9f29ac519c3020c63747dd3485b497d47f2436d8
naomijoshi <joshi.nao@husky.neu.edu>
2016-04-26 00:24:28 -0400

    Copied all installation scripts in one folder

--- UserInterface/installRuby.sh
+++
@@ -1,9 +0,0 @@
-
-	echo "Installing Ruby"
-	sudo apt-get install python-software-properties
-	sudo apt-add-repository -y ppa:brightbox/ruby-ng
-	sudo apt-get update
-	sudo apt-get install ruby2.3
-	sudo apt-get install ruby2.3-dev
-	sudo apt-get install ruby-switch
-
