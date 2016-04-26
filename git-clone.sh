36089b9028434d2014cd8914be39ffd042263caa
Naomi Joshi <naomi2610@gmail.com>
2016-04-26 15:19:09 +0000

    More updates to installation scripts

---
+++ git-clone.sh
@@ -0,0 +1,3 @@
+sudo apt-get update
+sudo apt-get install -y git
+git clone https://github.com/BU-NU-CLOUD-SP16/Lambda-on-OpenStack.git
--- install/python-package-installer.sh
+++ install/python-package-installer.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 sudo apt-get update -y
 sudo apt-get install -y python-dev python-pip
-pip install python-openstackclient
+sudo pip install python-openstackclient
 
-pip install ndg-httpsclient
-pip install pyasn1
+sudo pip install ndg-httpsclient
+sudo pip install pyasn1
 sudo apt-get install libffi-dev
 sudo apt-get update -y
 sudo apt-get install -y rabbitmq-server
-pip install pika
-pip install pymongo
+sudo pip install pika
+sudo pip install pymongo
 sudo apt-get install -y git
