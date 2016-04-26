f71c3ccd288b4c4bcbdd74e09ab2714f787b68fd
Sushant Mimani <sushant.mimani@gmail.com>
2016-04-26 04:16:02 -0400

    More script changes

--- install/ruby-package-installer.sh
+++ install/ruby-package-installer.sh
@@ -10,5 +10,5 @@ sudo apt-get install -y ruby-switch
 #installing gems
 sudo gem install bundler
 #cat /home/ubuntu/Lambda-On-OpenStack/install/Gemfile | tr "\r" "\n" > /home/ubuntu/Lambda-On-OpenStack/install/Gemfile
-cd /home/ubuntu/Lambda-On-OpenStack/UserInterface
-bundle install
+cd /home/ubuntu/Lambda-On-OpenStack/install
+bundle install --path /home/ubuntu/Lambda-On-OpenStack/UserInterface
